c02a8e359ea2ed59d8f8df16c97d04f44ec7b546
gorstop <gorstop@gmail.com>
2018-03-28 19:47:56 -0700

    change IP

--- service/main.go
+++ service/main.go
@@ -31,7 +31,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.203.233:9200"
+	ES_URL = "http://35.231.209.54:9200"
 )
 
 func main() {
